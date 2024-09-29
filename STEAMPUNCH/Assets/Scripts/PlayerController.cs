f0fd1e6095935531b8d59196f3685e12405a2154
lfp9381 <lfp9381@rit.edu>
2024-09-29 17:22:25 -0400

    Update PlayerController.cs
    Implemented a simple function to pause and resume the game by pressing ESCAPE.

--- STEAMPUNCH/Assets/Scripts/PlayerController.cs
+++ STEAMPUNCH/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState { Demo, Pause }
+
 public class PlayerController : MonoBehaviour
 {
     private float horizontal;
@@ -18,35 +20,60 @@ public class PlayerController : MonoBehaviour
     bool isStanding = false;
     Animator animator;
 
+    private GameState _state; // Game State enum
+
     private void Start()
     {
         animator = GetComponent<Animator>();
+        _state = GameState.Demo; // Currently, there are only two states, and the default state is the demo.
     }
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        if (horizontal != 0 && isStanding)
+        // The "Gameplay" State. All physics happens here.
+        if (_state == GameState.Demo)
         {
-            animator.SetFloat("xvelocity", 1);
-        }
-        if (horizontal == 0 && animator.GetFloat("xvelocity") == 1)
-        {
-            animator.SetFloat("xvelocity", 0);
-        }
+            // Allows player to pause by pressing ESC
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                _state = GameState.Pause;
+            }
 
-        if (Input.GetKey(KeyCode.Space) && IsGrounded())
-        {
-            rb.velocity += new Vector2(rb.velocity.x, jumpingPower) * Time.deltaTime;
-            isStanding = false;
-        }
+            horizontal = Input.GetAxisRaw("Horizontal");
+            if (horizontal != 0 && isStanding)
+            {
+                animator.SetFloat("xvelocity", 1);
+            }
+            if (horizontal == 0 && animator.GetFloat("xvelocity") == 1)
+            {
+                animator.SetFloat("xvelocity", 0);
+            }
 
-        if (Input.GetKey(KeyCode.Space) && rb.velocity.y > 0f)
-        {
-            rb.velocity += new Vector2(rb.velocity.x, rb.velocity.y * 0.5f) * Time.deltaTime;
-            isStanding = false;
-        }
+            if (Input.GetKey(KeyCode.Space) && IsGrounded())
+            {
+                rb.velocity += new Vector2(rb.velocity.x, jumpingPower) * Time.deltaTime;
+                isStanding = false;
+            }
 
-        Flip();
+            if (Input.GetKey(KeyCode.Space) && rb.velocity.y > 0f)
+            {
+                rb.velocity += new Vector2(rb.velocity.x, rb.velocity.y * 0.5f) * Time.deltaTime;
+                isStanding = false;
+            }
+
+            Flip();
+        }
+        // The Pause state. No updates or player control of any kind.
+        else if (_state == GameState.Pause)
+        {
+
+            // Allows player to resume by pressing ESC
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 1;
+                _state = GameState.Demo;
+            }
+        }
     }
 
     private void FixedUpdate()
