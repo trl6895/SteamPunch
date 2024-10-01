de9b661eadaa026b8ca1ba539cc4b6e34f9a8b0e
spb2528 <sbethel2022@gmail.com>
2024-10-01 17:39:35 -0400

    jumping animation added

--- STEAMPUNCH/Assets/Scripts/PlayerController.cs
+++ STEAMPUNCH/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
 
     //animation
+    [SerializeField]
     bool isStanding = false;
     Animator animator;
 
@@ -25,6 +26,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+
+        //walking animation
         if (horizontal != 0 && isStanding)
         {
             animator.SetFloat("xvelocity", 1);
@@ -46,12 +49,28 @@ public class PlayerController : MonoBehaviour
             isStanding = false;
         }
 
+        if (!IsGrounded())
+        {
+            animator.SetBool("IsJumping", true);
+        }
         Flip();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        
+        // jump/fall animation
+        if (!isStanding) 
+        {
+            animator.SetBool("IsJumping", true);
+            animator.SetFloat("yvelocity", rb.velocity.y);
+        }
+        else
+        {
+            animator.SetBool("IsJumping", false);
+        }
+
     }
 
     private bool IsGrounded()
