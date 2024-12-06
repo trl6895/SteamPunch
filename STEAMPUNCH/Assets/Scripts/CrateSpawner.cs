df775803fc3a03f53553b0afd0160430d704ef0c
Elias Duke <egd4546@rit.edu>
2024-12-06 01:02:14 -0500

    Added Button Prefab

--- STEAMPUNCH/Assets/Scripts/CrateSpawner.cs
+++ STEAMPUNCH/Assets/Scripts/CrateSpawner.cs
@@ -6,13 +6,11 @@ public class CrateSpawner : MonoBehaviour
 {
 
     [SerializeField] private GameObject crate;
-    List<GameObject> crateList;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -24,8 +22,7 @@ public class CrateSpawner : MonoBehaviour
 
     public void Activate()
     {
-        crateList.Add(crate);
-        Instantiate(crateList[0], new Vector3(transform.position.x, transform.position.y + gameObject.GetComponent<SpriteRenderer>().bounds.size.y / 2, transform.position.z), transform.rotation);
+        Instantiate(crate, new Vector3(transform.position.x, transform.position.y + gameObject.GetComponent<SpriteRenderer>().bounds.size.y / 2, transform.position.z), transform.rotation);
     }
 
 }
